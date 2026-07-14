949794ffbfc434d153b25c907d211327cd493ef9
Alberto Xamin <axamin@rippling.com>
2026-07-14 12:37:59 +0100

    add pipeline

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/ams-painter/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['manifold-3d'],
